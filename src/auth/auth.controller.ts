f2ad481509f9f897e1311c25ffe3b91e2d3e7a35
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2022-10-01 10:31:33 -0300

    ✨ initial files and settings

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Req() req: Request) {
+    console.log(req.body);
+
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
